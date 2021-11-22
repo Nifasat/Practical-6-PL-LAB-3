833f0732a888eff99c2d427b71bc709674a2e440
Nifasat <mohd.beg@walchandsangli.ac.in>
2021-11-22 21:00:10 +0530

    init

---
+++ SecondProblem.js
@@ -0,0 +1,13 @@
+5+4
+5%2
+5+(5/2-1)
+var name = "Nifasat";
+name;
+var rollNo = "2019BTECS00013";
+rollNo
+var x = 0;
+for(let i = 0;i<5;i++)
+{
+x++;
+console.log("Current Value of X is " + x);
+}
